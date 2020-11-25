acda86097b43fa5df6c907ba1fead8f36010fc79
Derek Groh <derekgroh@gmail.com>
2020-11-24 18:06:35 -0600

    clean up merge

--- resources/ctl.rb
+++ resources/ctl.rb
@@ -60,34 +60,6 @@ action :install do
       version new_resource.version unless new_resource.version == 'latest'
     end
   end
-
-  if platform?('windows')
-    # This is awaiting a packaged method to be delivered, but provides a resource currently.
-
-    unless shell_out('sensuctl version').stdout.match?("sensuctl version #{node['sensu-go']['ctl_version']}")
-      directory 'c:\sensutemp'
-
-      powershell_script 'Download Sensuctl' do
-        code "Invoke-WebRequest https://s3-us-west-2.amazonaws.com/sensu.io/sensu-go/#{node['sensu-go']['ctl_version']}/sensu-go_#{node['sensu-go']['ctl_version']}_windows_amd64.tar.gz  -OutFile c:/sensutemp/sensu-go_#{node['sensu-go']['ctl_version']}_windows_amd64.tar.gz"
-        not_if "Test-Path c:/sensutemp/sensu-go_#{node['sensu-go']['ctl_version']}_windows_amd64.tar.gz"
-        notifies :extract, 'archive_file[Extract Sensuctl]'
-      end
-
-      archive_file 'Extract Sensuctl' do
-        path "c:/sensutemp/sensu-go_#{node['sensu-go']['ctl_version']}_windows_amd64.tar.gz"
-        destination sensuctl_bin
-        overwrite true
-        action :nothing
-      end
-
-      windows_path sensuctl_bin
-
-      directory 'c:\sensutemp' do
-        action :delete
-        recursive true
-      end
-    end
-  end
 end
 
 action :configure do

4b1d70c648bee10082fbf88ad76e8f4cca8f31be
Derek Groh <derekgroh@gmail.com>
2020-12-28 12:52:23 -0600

    force choco to reload windows path

--- libraries/helpers_sensuctl.rb
+++ libraries/helpers_sensuctl.rb
@@ -34,6 +34,30 @@ module SensuCookbook
       def sensuctl_asset_update_cmd
         [sensuctl_bin, 'asset', 'update', new_resource.name, sensuctl_asset_update_opts].flatten
       end
+
+      def get_reg_env(hkey, subkey, &block)
+        Win32::Registry.open(hkey, subkey) do |reg|
+          reg.each_value do |name|
+            value = reg.read_s_expand(name)
+            ENV[name] = if block && ENV.key?(name)
+                          block.call(name, ENV[name], value)
+                        else
+                          ENV[name] = value
+                        end
+          end
+        end
+      end
+
+      def refresh_env
+        get_reg_env(Win32::Registry::HKEY_LOCAL_MACHINE, 'System\CurrentControlSet\Control\Session Manager\Environment')
+        get_reg_env(Win32::Registry::HKEY_CURRENT_USER, 'Environment') do |name, old_value, new_value|
+          if name.upcase == 'PATH'
+            old_value || File::PATH_SEPARATOR || new_value
+          else
+            new_value
+          end
+        end
+      end
     end
   end
 end
